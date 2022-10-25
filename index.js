c4b000ca39eacdd9e9b40af0a55f74e78cdab083
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-26 04:23:52 +0600

    couse-category data send

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) => {
   res.send('Courses API running');
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
   res.send(categories);
+})
+
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCategory = course.find(c => c.category_id === id);
+  res.send(selectedCategory);
 })
 
 app.listen(port, () => {
